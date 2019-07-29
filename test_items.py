0760a2ac460def3ab44a4acf21fcb49168ca7e1f
oliviacolin <oliviacolin@developmentontheedge.com>
2019-07-29 14:34:00 +0700

    v.N

--- test_items.py
+++ test_items.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 def test_link(lang, browser):
 
@@ -6,6 +7,7 @@ def test_link(lang, browser):
 #    print(link)
     browser.get(link)
     button = browser.find_element_by_class_name("btn-add-to-basket")
+#    time.sleep(30)
     assert button
     browser.quit()
    
