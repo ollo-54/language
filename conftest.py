2b21db836715c6a59bea6535174df52bd0f85544
oliviacolin <oliviacolin@developmentontheedge.com>
2019-07-29 14:07:57 +0700

    v.N

--- conftest.py
+++ conftest.py
@@ -3,12 +3,12 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
 def pytest_addoption(parser):
-    parser.addoption('--lang', action='store', default='ru')
+    parser.addoption('--language', action='store', default='ru')
 
 @pytest.fixture(scope="function")
 
 def lang(request):
-    br_lang = request.config.option.lang
+    br_lang = request.config.option.language
     if br_lang is None:
         pytest.skip()
     return br_lang
