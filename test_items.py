40497cbc81f8e8d785e6e673ef308f5009b3b539
oliviacolin <oliviacolin@developmentontheedge.com>
2019-07-30 15:01:16 +0700

    v.N

--- test_items.py
+++ test_items.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 import time
 
-def test_link_language(lang, browser):
+def test_link_language(test_lang, test_browser):
 
-    link = ("http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/".format(lang))
+    link = ("http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/".format(test_lang))
 #    print(link)
-    browser.get(link)
-    button = browser.find_element_by_class_name("btn-add-to-basket")
+    test_browser.get(link)
+    button = test_browser.find_element_by_class_name("btn-add-to-basket")
 #    time.sleep(30)
     assert button
-    browser.quit()
+    test_browser.quit()
    
